8e6f91cb8dea52849da49b765907ef31cb7dec8a
sebascoe99 <sebastian.coello1@gmail.com>
2022-02-06 16:20:09 -0500

    arreglo de las rutas

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+
+/*
 Route::middleware(['auth:sanctum'])->group(function () {
     // Rutas para el controlador ProductoController
     //Route::get('/products', [ProductoController::class, 'index']); //mostrar todos los registros
@@ -25,11 +26,19 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
     // Rutas para el controlador ProductoCategoriaController
     Route::post('/productCategory', 'App\Http\Controllers\ProductoCategoriaController@store'); //crear un registro
-});
-    Route::get('/products', [ProductoController::class, 'index']); //mostrar todos los registros
-/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
 });*/
+   // Rutas para el controlador ProductoController
+    Route::get('/products', [ProductoController::class, 'index']); //mostrar todos los registros
+    Route::post('/products', [ProductoController::class, 'store']); //crear un registro
+    Route::put('/products/{id}', [ProductoController::class, 'update']); //actualizar un registro
+    Route::delete('/products/{id}', 'App\Http\Controllers\ProductoController@destroy'); //eliminar un registro
+
+    // Rutas para el controlador ProductoCategoriaController
+    Route::post('/productCategory', 'App\Http\Controllers\ProductoCategoriaController@store'); //crear un registro
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 Route::post('/register', 'App\Http\Controllers\AuthController@register');
 Route::post('/login', 'App\Http\Controllers\AuthController@login');
